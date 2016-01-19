bb293bed1bde4510f93f711575ebe2e76fbd5d0b
Federico <undicizeri@gmail.com>
2016-01-19 08:37:51 +0100

    Cambiata la gestione dei file aggiungendo la classe GestoreFile

---
+++ ConsoleApplication1/GestoreFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class GestoreFile
+    {
+        public string PathTo;
+        public string PathFrom;
+        public string Nome;
+        public string NomeConEstensione;
+
+        public GestoreFile(string path, string estensione)
+        {
+            Nome = path;
+            NomeConEstensione = path +"." + estensione;
+            PathTo = @"C:\Users\admin\Documents\Visual Studio 2015\Projects\RisultatoPolarityTwitch\Files\RisultatiPerGiorno";
+            PathFrom = @"C:\Users\admin\Documents\Visual Studio 2015\Projects\RisultatoPolarityTwitch\Files\";
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -9,11 +9,14 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static string NOMEFILE = @"1_7_massansc.txt";
-        static string NOMEFILELESS = NOMEFILE + @".less4";
-        static string OUTPUT_PATH = @"C:\Users\admin\Documents\Visual Studio 2015\Projects\RisultatoPolarityTwitch\Files\output.txt";
-        static string BASELINE_PATH = @"C:\Users\admin\Documents\Visual Studio 2015\Projects\RisultatoPolarityTwitch\Files\baseline.txt";
-        static string PATH = @"C:\Users\admin\Documents\Visual Studio 2015\Projects\RisultatoPolarityTwitch\Files\";
+        static GestoreFile Input = new GestoreFile("1_7_massansc", "txt");
+        static GestoreFile InputLess4 = new GestoreFile(Input.NomeConEstensione, @".less4");
+        static GestoreFile InputRnnlm = new GestoreFile("TESTRES", "txt");
+        static GestoreFile InputBOW = new GestoreFile("BOW", "txt");
+        //static string OUTPUT_PATH = PATH + @"output.txt";
+        //static string BASELINE_PATH = PATH + @"baseline.txt";
+        //static string TEST_PATH = PATH + @"TESTRES.txt";
+        static GestoreFile OutputXLS = new GestoreFile(Input.Nome, "xlsx");
 
         static void Main(string[] args)
         {
@@ -22,36 +25,50 @@ namespace ConsoleApplication1
             ProcessaRes bowRes = new ProcessaRes();
 
             //pRes.SeparaGiorni();
-            string[] lines = File.ReadAllLines(@"C:\Users\admin\Documents\Visual Studio 2015\Projects\RisultatoPolarityTwitch\Files\TESTRES.txt");
+            //Calcolo test + cbow
+            string[] lines = File.ReadAllLines(InputRnnlm.PathFrom);
             pRes.Process(lines);
-            lines = File.ReadAllLines(PATH + NOMEFILELESS);
+            lines = File.ReadAllLines(InputLess4.PathFrom);
             pRes.Cbow(lines);
 
             //Calcolo BOW
-            lines = File.ReadAllLines(@"C:\Users\admin\Documents\Visual Studio 2015\Projects\RisultatoPolarityTwitch\Files\bow.txt");
+            lines = File.ReadAllLines(InputBOW.PathFrom);
             bowRes.ProcessBow(lines);
 
             //Calcolo baseline
-            lines = File.ReadAllLines( PATH + NOMEFILE);
+            lines = File.ReadAllLines(Input.PathFrom);
             baseLineRes.Cbow(lines);
-            lines = File.ReadAllLines(PATH + NOMEFILELESS);
+            lines = File.ReadAllLines(InputLess4.PathFrom);
             baseLineRes.Cbow(lines);
             //Sposta Directory
 
             StampaExcel stampa = new StampaExcel();
-            stampa.CreaExcel(pRes.giornata, baseLineRes.giornata, bowRes.giornata, PATH + NOMEFILE + @".xlsx");
-            //Termina
+            stampa.CreaExcel(pRes.giornata, baseLineRes.giornata, bowRes.giornata, OutputXLS.PathFrom);
+            //Sposta i files
+            //if(!Directory.Exists(PATH_RISULTATI_DIR))
+            //{
+            //    Directory.CreateDirectory(PATH_RISULTATI_DIR);
+            //}
+            //SpostaFile(TEST_PATH, PATH_RISULTATI_DIR + "")
+            
+            //string path = PATH + NOMEFILE
             Console.ReadKey();
         }
 
+        static void SpostaFile(string pathFrom, string pathTo)
+        {
+            if (File.Exists(pathTo))
+                File.Delete(pathTo);
+            File.Move(pathFrom, pathTo);
+        }
         static void ContaOccorenze()
         {
             ProcessaRes pRes = new ProcessaRes();
-            string[] lines = File.ReadAllLines(PATH + NOMEFILE + @".txt");
+            string[] lines = File.ReadAllLines(Input.PathFrom);
             pRes.ContaEmoticons(lines);
             Console.WriteLine("Emoticons positive {0} Negative {1} righe totali {2}", pRes.numPos, pRes.numNeg, pRes.numRighe);
             //baseLineRes.Cbow(lines);
-            lines = File.ReadAllLines(PATH + NOMEFILE + @".txt.less4");
+            lines = File.ReadAllLines(InputLess4.PathFrom);
             pRes.ContaEmoticons(lines);
             Console.WriteLine("Emoticons positive {0} Negative {1} righe totali {2}", pRes.numPos, pRes.numNeg, pRes.numRighe);
         }
